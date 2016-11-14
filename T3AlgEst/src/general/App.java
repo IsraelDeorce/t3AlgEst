ca5309dc1f82a8871e3af79c7da530b7ea9447d7
Israel Deorce <israel.idvj@gmail.com>
2016-11-14 11:32:12 -0200

    Commit Inicial

---
+++ T3AlgEst/src/general/App.java
@@ -0,0 +1,82 @@
+package general;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App {
+	
+	public static void main(String[] args) throws IOException{
+		
+		Arvore arvore = new Arvore();
+		boolean livroUnico = false;
+		
+			
+		//===================================================================================
+		//Gera os 100 nomes do txt e armazena em um vetor para ser utilizado no gerador de clientes			
+		BufferedReader in = new BufferedReader(new FileReader("livro.txt")); 
+		for (int i=0; in.ready(); i++) { 			
+			String linha = in.readLine();
+			switch(linha.charAt(0)){
+				case "L":
+					if(livroUnico)
+						throw new RuntimeException("só pode haver um livro");
+					livroUnico = true;
+					arvore
+				
+			
+			
+			
+			
+			}		
+			
+			
+			
+			
+		}
+		in.close();	//fecha o arquivo	
+		//===================================================================================			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/* TESTES QUE FUNCIONAM (precisa dos TADS Integer e não String:
+		 * 
+		GeneralTreeOfString arvore = new  GeneralTreeOfString();
+		arvore.add(15, null);
+		arvore.add(23, 15);		
+		arvore.add(12, 15);	
+		arvore.add(8, 15);	
+		arvore.add(5, 23);	
+		arvore.add(18, 23);	
+		arvore.add(17, 12);	
+		arvore.add(7, 8);	
+		arvore.add(25, 8);	
+		arvore.add(19, 8);
+		arvore.add(6, 8);
+		
+		LinkedListOfInteger lista1 = arvore.positionsPre();
+		LinkedListOfInteger lista2 = arvore.positionsPos();
+		LinkedListOfInteger lista3 = arvore.positionsWidth();
+		
+		System.out.println("Posições Pré:");
+		System.out.println(lista1.toString());
+		System.out.println("Posições Pós:");
+		System.out.println(lista2.toString());
+		System.out.println("Posições Width:");
+		System.out.println(lista3.toString());	
+		*/
+	}	
+}
---
+++ T3AlgEst/src/general/GeneralTreeOfString.java
@@ -0,0 +1,229 @@
+package general;
+
+public class GeneralTreeOfString {	
+	
+	private Node root;
+	private int count;	
+	
+	protected static final class Node{
+		
+		public Node father;
+		public String element;
+		public LinkedListOfNodes subtrees;
+		
+		public Node(String element){
+			
+			this.father = null;
+			this.element = element;
+			subtrees = new LinkedListOfNodes();
+		}
+		
+		public void addSubtree(Node n){
+			n.father = this;
+			subtrees.add(n);					
+		}		
+		
+		public boolean removeSubtree(Node n){
+			n.father = null;
+			return subtrees.remove(n);
+		}
+		
+		public Node getSubtree(int i) {
+			if(i<0 || i>=subtrees.size())				
+				throw new RuntimeException("Index inválido");
+			return subtrees.get(i);				
+		}
+		
+		public int getSubtreeSize(){
+			return subtrees.size();
+		}
+		@Override
+		public String toString() {
+			return "Node [father=" + father + ", element=" + element + ", subtrees=" + subtrees + "]";
+		}	
+	}
+	
+	public GeneralTreeOfString(){
+		this.root = null;
+		this.count = 0;
+	}
+	public GeneralTreeOfString(String element){
+		this.root = new Node(element);
+		this.count = 1;
+	}
+	
+	//Retorna referencia de um nodo que possua determinado elemento a partir de uma arvore/subarvore
+	private Node searchNodeRef(String element, Node target){
+		Node n = null;
+		if(target != null){
+			if(element.equals(target.element))
+				n = target;
+			else{
+				int i=0;
+				Node aux = null;
+				while(aux==null && i<target.getSubtreeSize()){
+					aux = searchNodeRef(element, target.getSubtree(i));
+					i++;
+				}
+				n=aux;
+			}
+		}
+		return n;	
+	}
+	
+	// insere o elemento e como	filho de father
+	public boolean add(String element, String father){
+		Node n = new Node(element);
+		if(father==null){//se for nulo, é para adicionar na raiz
+			if(root!=null){	
+				root.father = n;
+				n.addSubtree(root);
+			}
+			root = n;			
+		}
+		//se father não é nulo, é para adicionar como subtree dele
+		else{
+			Node aux = searchNodeRef(father, root);
+			if(aux==null)
+				return false;
+			aux.addSubtree(n);
+			n.father = aux;		
+		}
+		count++;
+		return true;		
+	}
+	
+	// retorna o elemento armazenado na raiz
+	public String getRoot(){
+		if(root==null)
+			throw new RuntimeException("Raíz nula");			
+		return root.element;
+	}
+	
+	//altera o elemento armazenado na raiz
+	public void setRoot(String e){
+		if(root==null)
+			add(e, null);
+		root.element = e;						
+	}
+	
+	//retorna o pai do elemento e
+	public String getParent(String e){
+		Node n = searchNodeRef(e, root);
+		if(n==null)
+			throw new RuntimeException("Elemento não encontrado!");
+		return n.father.element;
+	}
+	
+	// remove o elemento e e seus filhos
+	public boolean removeBranch(String e){
+		Node n = searchNodeRef(e, root);	
+		if(n==null)
+			 return false;
+		count = count - n.getSubtreeSize();
+		Node father = n.father;
+		father.removeSubtree(n);
+		return true;
+	}
+	
+	//retorna true se a árvore contém o	elemento
+	public boolean contains(String e){
+		if(searchNodeRef(e,root)!=null)
+			return true;
+		return false;
+	}
+	
+	// retorna true se o elemento está armazenado em um nodo interno
+	public boolean isInternal(String e){
+		Node n = searchNodeRef(e, root);
+		if(n!=null){
+			return n.getSubtreeSize()>0;
+		}
+		return false;
+	}
+	
+	// retorna true se o elemento está armazenado em um nodo externo
+	public boolean isExternal(String e){
+		Node n = searchNodeRef(e, root);
+		if(n!=null){
+			return n.getSubtreeSize()==0;
+		}
+		return false;
+	}
+	
+	//retorna true se o elemento e está	armazenado na raiz
+	public boolean isRoot(String e){
+		if(e!=null){
+			return root.element == e;
+		}
+		return false;
+	}
+	
+	//retorna true se a árvore está vazia
+	public boolean isEmpty(){
+		return count==0;
+	}
+	
+	//retorna o número de elementos armazenados na árvore
+	public int size(){
+		return count;
+	}
+	
+	//remove todos os elementos da árvore
+	public void clear(){
+		root = null;
+		count = 0;
+	}
+	
+	//retorna uma lista com todos os elementos da árvore na ordem pré-fixada
+	public LinkedListOfString positionsPre(){
+		LinkedListOfString lista = new LinkedListOfString();
+		positionsPreAux(root, lista);
+		return lista;		
+	}
+	public void positionsPreAux(Node n, LinkedListOfString lista){
+		if(n!=null){
+			lista.add(n.element);
+			for(int i=0; i<n.getSubtreeSize();i++){
+				positionsPreAux(n.getSubtree(i),lista);
+			}			
+		}
+	}
+	
+	//retorna uma lista com todos os elementos da árvore na ordem pos-fixada
+	public LinkedListOfString positionsPos(){
+		LinkedListOfString lista = new LinkedListOfString();
+		positionsPosAux(root, lista);
+		return lista;		
+	}
+	public void positionsPosAux(Node n, LinkedListOfString lista){
+		if(n!=null){			
+			for(int i=0; i<n.getSubtreeSize();i++){
+				positionsPosAux(n.getSubtree(i),lista);
+			}
+			lista.add(n.element);
+		}
+	}
+	
+	// retorna uma lista com todos os elementos da árvore com um caminhamento em largura
+	public LinkedListOfString positionsWidth(){
+		LinkedListOfString lista = new LinkedListOfString();
+		if(root!=null){
+			LinkedQueueOfNodes fila = new LinkedQueueOfNodes();
+			Node aux = root;
+			fila.enqueue(aux);
+			while(!fila.isEmpty())
+				aux=fila.dequeue();
+				lista.add(aux.element);
+				for(int i=0; i<aux.getSubtreeSize(); i++)
+					fila.enqueue(aux.getSubtree(i));			
+		}
+		return lista;
+	}
+
+	@Override
+	public String toString() {
+		return "GeneralTreeOfInteger [root=" + root + ", count=" + count + "]";
+	}
+		
+}
