31fceb4b091b1e5cbce99cd01523130a77eee36e
Israel Deorce <israel.idvj@gmail.com>
2016-11-27 19:18:26 -0200

    Release 1.0

--- T3AlgEst/src/Isradois/App.java
+++ T3AlgEst/src/Isradois/App.java
@@ -21,6 +21,7 @@ public class App {
 		
 		arvore = new GeneralTreeOfString();
 		lista = new LinkedListOfString();
+		sumario = new LinkedListOfString();
 		
 		/*================================================================================*/
 		System.out.print("Carregando arquivo livro.txt... ");
@@ -40,41 +41,13 @@ public class App {
 		}
 		else
 			throw new RuntimeException("Erro ao gerar a árvore");
-		System.out.print("Gerando o Sumário...");		
-		sumario = gerarSumario();
-		if(sumario!=null){
-			System.out.println("ok\n");
-			printaSumario(sumario);
-		}
-		else
-			throw new RuntimeException("Erro ao gerar o sumário");	
 		
-		geraDocumento();
-		
-
+		/*================================================================================*/
+		System.out.print("Gerando o Sumário e gravando o Livro...");		
+		if(geraDocumento())
+			System.out.print("ok\n");
 	}
-		public void escondeComents(){	
-		/*
-		LinkedListOfString lista1 = arvore.positionsPre();
-		LinkedListOfString lista2 = arvore.positionsPos();
-		LinkedListOfString lista3 = arvore.positionsWidth();
-		LinkedListOfString lista4 = arvore.positions();
-		
-		System.out.println("Posições Pré:");
-		System.out.println(lista1.toString());
-		System.out.println("Posições Pós:");
-		System.out.println(lista2.toString());
-		System.out.println("Posições Width:");
-		System.out.println(lista3.toString());	
-		System.out.println("Posições:");
-		System.out.println(lista4.toString());
-		
-							
-				Gerando o sumário... ok
-				Imprimindo o livro para o arquivo livro_prod.txt... ok.
-			*/
-		}
-		
+	
 	/* 1. Guardar a estrutura do livro em uma árvore; */
 	public static boolean lerArquivo(String arquivo) throws FileNotFoundException, IOException{
 		
@@ -83,57 +56,116 @@ public class App {
 				lista.add(in.readLine());
 			return true;
 		}
-	}
+	}	
 	
 	/* 2. Gerar o sumário do livro, indicando cada capítulo, cada seção e subseção com os respectivos 
-	números de página (assumindo que cada página suporte 15 linhas de texto); */
+	números de página (assumindo que cada página suporte 15 linhas de texto); */	
+	/* 3. “Imprimir” (gravar) o livro em um arquivo texto de saída, observando o formato a seguir.*/	
+	public static boolean geraDocumento() throws UnsupportedEncodingException, FileNotFoundException, IOException{	
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(				
+	       new FileOutputStream("livro_prod.txt"), "utf-8"))) {
+				writer.write("--------------------------------------------");
+				for(int i=1; i<=15; i++){
+					writer.write("\n"+i);
+					if(i==7)
+						writer.write("               "+livro.get(0)+"               ");
+					if(i==15)
+						writer.write("\n-------------------------------------- Capa");
+				}	
+				
+				int capitulo = 0, secao = 0, subsecao = 0;
+				String linha = "", tipo = "", numeracao = "";				
+				int pag = 1;	
+				
+				for(int i=0, j=0; i<lista.size(); i++){								
+							linha = livro.get(i);							
+							tipo = linha.substring(0, 2);
+							switch(tipo){
+							case "C ":
+								j++;								
+								capitulo++;
+								secao = 0;
+								subsecao = 0;
+								numeracao = String.valueOf(capitulo) + ". ";									
+								writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));
+								String pontinhosC = linha.replace(tipo, numeracao);
+								while(pontinhosC.length()<40){
+									pontinhosC = pontinhosC + "."	;								
+								}								
+								sumario.add(pontinhosC + pag);
+								if(j>=15){
+									j=0;									
+									writer.write("\n-------------------------------------- pg."+pag);
+									pag++;
+								}								
+								break;						
+							case "S ":
+								j++;
+								secao++;
+								subsecao = 0;
+								numeracao = String.valueOf(capitulo) + "."
+											+ String.valueOf(secao) + ". ";	
+								writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));
+								String pontinhosS = linha.replace(tipo, numeracao);
+								while(pontinhosS.length()<40){
+									pontinhosS = pontinhosS + "."	;								
+								}
+								sumario.add(pontinhosS + pag);
+								if(j>=15){
+									j=0;									
+									writer.write("\n-------------------------------------- pg."+pag);	
+									pag++;
+								}
+								break;						
+							case "SS":
+								j++;
+								subsecao++;
+								numeracao = String.valueOf(capitulo) + "."
+										+ String.valueOf(secao) + "."
+										+ String.valueOf(subsecao) + ".";	
+								writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));
+								String pontinhosSS = linha.replace(tipo, numeracao);
+								while(pontinhosSS.length()<40){
+									pontinhosSS = pontinhosSS + "."	;								
+								}								
+								sumario.add(pontinhosSS + pag);
+								if(j>=15){
+									j=0;									
+									writer.write("\n-------------------------------------- pg."+pag);	
+									pag++;
+								}
+								break;						
+							case "P ":
+								for(int k=0; k < Integer.parseInt(livro.get(i).substring(2, livro.get(i).length()));){
+									j++;
+									k++;										
+									writer.write("\n"+j+"  "+"Lorem Ipsum "+k);
+									if(j>=15){
+										j=0;										
+										writer.write("\n-------------------------------------- pg."+pag);
+										pag++;
+									}
+								}									
+							default:
+								break;
+							}														
+				}				
+				writer.write("\n-------------------------------------- pg."+pag);
+				writer.write("\nSUMARIO");
+				for(int i=0; i<sumario.size();i++){					
+					writer.write("\n"+sumario.get(i));				
+				}
+			}	
+		return true;
+	}	
 	
-	public static LinkedListOfString gerarSumario(){		
-		if(livro==null) return null;
-		LinkedListOfString sumario = new LinkedListOfString();
-		int capitulo = 0, secao = 0, subsecao = 0;
-		String linha = "", tipo = "", numeracao = "";
-		for(int i = 0; i<livro.size();i++){
-			linha = livro.get(i);
-			tipo = linha.substring(0, 2);
-			switch(tipo){
-			case "C ":
-				capitulo++;
-				secao = 0;
-				subsecao = 0;
-				numeracao = String.valueOf(capitulo) + ". ";
-				sumario.add(linha.replace(tipo, numeracao)); 
-				break;
-			case "S ":
-				secao++;
-				subsecao = 0;
-				numeracao = String.valueOf(capitulo) + "."
-							+ String.valueOf(secao) + ". ";
-				sumario.add(linha.replace(tipo, numeracao));
-				break;
-			case "SS":
-				subsecao++;
-				numeracao = String.valueOf(capitulo) + "."
-						+ String.valueOf(secao) + "."
-						+ String.valueOf(subsecao) + ".";
-			sumario.add(linha.replace(tipo, numeracao));
-				break;
-			default:
-				break;
-			}			
-		}
-		return sumario;		
-	}
-	
-	public static void printaSumario(LinkedListOfString sumario){
+	public static void printaSumario(){
+		if(sumario.isEmpty())
+			return;
 		for(int i = 0; i<sumario.size(); i++){
 			System.out.println(sumario.get(i));			
 		}
-	}
-	
-	
-	/* 3. “Imprimir” (gravar) o livro em um arquivo texto de saída, observando o formato a seguir.*/
-	
+	}	
 	
 	/*Printa na tela a quantidade de capitulos, secoes, subsecoes e paragrafos presentes no livro*/
 	public static void printaQuantidade(){
@@ -158,127 +190,23 @@ public class App {
 			+ "\nSeções......: " + secoes
 			+ "\nSubseções...: " + subsecoes
 			+ "\nParágrafos..: " + paragrafos);			
-	}	
-
-	public static void geraNumeracao(){
-		
 	}
 	
-	public static void geraDocumento() throws UnsupportedEncodingException, FileNotFoundException, IOException{
+	public void escondeComents(){	
+		/*
+		LinkedListOfString lista1 = arvore.positionsPre();
+		LinkedListOfString lista2 = arvore.positionsPos();
+		LinkedListOfString lista3 = arvore.positionsWidth();
+		LinkedListOfString lista4 = arvore.positions();
 		
-		
-		try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-				
-	       new FileOutputStream("livro_prod.txt"), "utf-8"))) {
-				writer.write("--------------------------------------------");
-				for(int i=1; i<=15; i++){
-					writer.write("\n"+i);
-					if(i==7)
-						writer.write("               "+livro.get(0)+"               ");
-					if(i==15)
-						writer.write("\n-------------------------------------- Capa");
-				}		
-				
-				
-				int capitulo = 0, secao = 0, subsecao = 0, paragrafo = 0;
-				String linha = "", tipo = "", numeracao = "";				
-				int pag = 0;				
-				
-				for(int i=0, j=0; i<lista.size(); i++){								
-							linha = livro.get(i);							
-							tipo = linha.substring(0, 2);
-							switch(tipo){
-							case "C ":
-								j++;								
-								capitulo++;
-								secao = 0;
-								subsecao = 0;
-								paragrafo = 0;
-								numeracao = String.valueOf(capitulo) + ". ";									
-								writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));	
-								if(j>=15){
-									j=0;
-									pag++;
-									writer.write("\n-------------------------------------- pg."+pag);
-								}
-								break;
-							case "S ":
-								j++;
-								secao++;
-								subsecao = 0;
-								paragrafo = 0;
-								numeracao = String.valueOf(capitulo) + "."
-											+ String.valueOf(secao) + ". ";	
-								writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));
-								if(j>=15){
-									j=0;
-									pag++;
-									writer.write("\n-------------------------------------- pg."+pag);
-								}
-								break;
-							case "SS":
-								j++;
-								subsecao++;
-								paragrafo = 0;
-								numeracao = String.valueOf(capitulo) + "."
-										+ String.valueOf(secao) + "."
-										+ String.valueOf(subsecao) + ".";	
-								writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));
-								if(j>=15){
-									j=0;
-									pag++;
-									writer.write("\n-------------------------------------- pg."+pag);
-								}
-								break;
-							case "P ":
-								for(int k=0; k < Integer.parseInt(livro.get(i).substring(2, livro.get(i).length()));){
-									j++;
-									k++;										
-									writer.write("\n"+j+"  "+"Lorem Ipsum "+k);
-									if(j>=15){
-										j=0;
-										pag++;
-										writer.write("\n-------------------------------------- pg."+pag);
-									}
-								}									
-								paragrafo++;
-							default:
-								break;
-							}	
-														
-				}
-				pag++;
-				writer.write("\n-------------------------------------- pg."+pag);
-				writer.write("\nSUMARIO");
-				for(int i=0; i<sumario.size();i++){					
-					writer.write("\n"+sumario.get(i));				
-				}
-			}		
-	}	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		System.out.println("Posições Pré:");
+		System.out.println(lista1.toString());
+		System.out.println("Posições Pós:");
+		System.out.println(lista2.toString());
+		System.out.println("Posições Width:");
+		System.out.println(lista3.toString());	
+		System.out.println("Posições:");
+		System.out.println(lista4.toString());			
+			*/
+		}
 }
