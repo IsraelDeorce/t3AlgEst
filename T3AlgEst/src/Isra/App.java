fc072d723e1c4c8c5243ae67145954779a9d3d75
DELL <DELL@DELL-PC.home>
2016-11-27 16:23:44 -0200

    Cansei

--- T3AlgEst/src/Isra/App.java
+++ T3AlgEst/src/Isra/App.java
@@ -1,20 +1,28 @@
 package Isra;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 
 public class App {
 	
 	private static GeneralTreeOfString arvore;
 	private static LinkedListOfString lista;
 	private static LinkedListOfString livro;
+	private static LinkedListOfString livroPronto;
+	private static LinkedListOfString sumario;
 	
 	public static void main(String[] args) throws IOException{
 		
 		arvore = new GeneralTreeOfString();
 		lista = new LinkedListOfString();
+		livroPronto = new LinkedListOfString();
 		
 		/*================================================================================*/
 		System.out.print("Carregando arquivo livro.txt... ");
@@ -35,13 +43,72 @@ public class App {
 		else
 			throw new RuntimeException("Erro ao gerar a árvore");
 		System.out.print("Gerando o Sumário...");		
-		LinkedListOfString sumario = gerarSumario();
+		sumario = gerarSumario();
 		if(sumario!=null){
 			System.out.println("ok\n");
-			printaSumario(sumario);
+			//printaSumario(sumario);
 		}
 		else
-			throw new RuntimeException("Erro ao gerar o sumário");		
+			throw new RuntimeException("Erro ao gerar o sumário");
+		
+		geraPaginas();
+		System.out.println(livroPronto);
+	}
+	
+	
+		
+		public static void geraDocumento() throws UnsupportedEncodingException, FileNotFoundException, IOException{
+			try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+		       new FileOutputStream("livro_prod.txt"), "utf-8"))) {
+					writer.write("--------------------------------------------");
+					for(int i=1; i<=umaPagina; i++){
+						writer.write("\n"+i);
+						if(i==7)
+							writer.write("               "+livro.get(0)+"               ");
+						if(i==15)
+							writer.write("\n-------------------------------------- Capa");
+					}				
+						for(int i=1; i<=livro.size(); i++){											
+							int capitulo = 0, secao = 0, subsecao = 0, paragrafo = 0;
+							String linha = "", tipo = "", numeracao = "";
+							for(int j=0; j<=umaPagina;j++){							
+								linha = livro.get(j);
+								tipo = linha.substring(0, 2);
+								switch(tipo){
+								case "C ":
+									capitulo++;
+									secao = 0;
+									subsecao = 0;
+									paragrafo = 0;
+									numeracao = String.valueOf(capitulo) + ". ";	
+									writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));								
+									break;
+								case "S ":
+									secao++;
+									subsecao = 0;
+									paragrafo = 0;
+									numeracao = String.valueOf(capitulo) + "."
+												+ String.valueOf(secao) + ". ";	
+									writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));
+									break;
+								case "SS":
+									subsecao++;
+									paragrafo = 0;
+									numeracao = String.valueOf(capitulo) + "."
+											+ String.valueOf(secao) + "."
+											+ String.valueOf(subsecao) + ".";	
+									writer.write("\n"+j+"  "+linha.replace(tipo, numeracao));								
+									break;
+								case "P ":
+									paragrafo++;								
+									writer.write("\n"+j+"  "+"Lorem Ipsum "+paragrafo);
+								default:
+									break;
+								}			
+							}
+							writer.write("\n-------------------------------------- pg."+i);						
+						}			
+				}
 
 	}
 	
@@ -97,7 +164,7 @@ public class App {
 				ordem = String.valueOf(capitulo) + ". ";
 				pagina++;
 				linha=1;
-				livroPronto.add("texto.replace(tipo, ordem));				
+				livroPronto.add(texto.replace(tipo, ordem));				
 				break;
 			case "S ":
 				secao++;
@@ -155,11 +222,72 @@ public class App {
 			+ "\nParágrafos..: " + paragrafos);			
 	}	
 
-	public static void geraNumeracao(LinkedListOfString sumario){
+	public static void geraPaginas(){
 		if(livro==null) return;
-		int pagina = 0, linha = 0;
+		int capitulo = 0, secao = 0, subsecao = 0, pagina = 0, linha = 1;
+		String texto = "", tipo = "", ordem = "";
 		
-		
-		
+		for(int i = 0; i<livro.size();i++){
+			texto = livro.get(i);
+			tipo = texto.substring(0, 2);
+			
+			switch(tipo){
+			
+			case "C ":
+				capitulo++;
+				secao = 0;
+				subsecao = 0;
+				ordem = String.valueOf(capitulo) + ". ";
+				pagina++;
+				linha=1;
+				sumario.add(texto.replace(tipo, ordem) + "........ " + pagina);
+				livroPronto.add(linha + " " + texto.replace(tipo, ordem));
+				linha++;
+				break;
+			
+			case "S ":
+				secao++;
+				subsecao = 0;
+				ordem = String.valueOf(capitulo) + "."
+							+ String.valueOf(secao) + ". ";
+				if(linha<15)
+					linha++;
+				else{
+					linha=1;
+					pagina++;
+				}
+				sumario.add("\t" + texto.replace(tipo, ordem) + "........ " + pagina);
+				livroPronto.add(linha + " " + texto.replace(tipo, ordem));
+				break;
+			
+			case "SS":
+				subsecao++;
+				ordem = String.valueOf(capitulo) + "."
+						+ String.valueOf(secao) + "."
+						+ String.valueOf(subsecao) + ".";
+				if(linha<15)
+					linha++;
+				else{ 
+					linha=1;
+					pagina++;
+				}
+				sumario.add("\t\t" + texto.replace(tipo, ordem) + "........ " + pagina);
+				livroPronto.add(linha + " " + texto.replace(tipo, ordem));
+				break;
+			
+			case "P ":
+				int tamanho = Integer.parseInt(texto.replaceAll("P ",""));				
+				for(int j = 1; j <tamanho; j++){
+					livroPronto.add(linha + " Lorem Ipsum " + j);
+					if(linha<15)
+						linha++;
+					else{ 
+						linha=1;
+						pagina++;
+					}
+				}
+				break;
+			}
+		}
 	}
 }
