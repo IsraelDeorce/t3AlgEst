d38a020a28c2634f41016f188e0628b88966b404
Israel Deorce <israel.idvj@gmail.com>
2016-11-27 00:38:58 -0200

    comit nope

--- T3AlgEst/src/general/App.java
+++ T3AlgEst/src/general/App.java
@@ -1,8 +1,10 @@
 package general;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.plaf.synth.SynthSeparatorUI;
 
@@ -14,51 +16,27 @@ public class App {
 	private static String secao;
 	private static String subsecao;
 	private static String paragrafo;
+	private static ArrayList<String> lista;
+	private static int pos = 0;
 	
 	public static void main(String[] args) throws IOException{
 		
 		arvore = new GeneralTreeOfString();
-		boolean livroUnico = false;
+		//boolean livroUnico = false;
+		
 		
 			
 		//===================================================================================
 		//Gera os 100 nomes do txt e armazena em um vetor para ser utilizado no gerador de clientes			
-		BufferedReader in = new BufferedReader(new FileReader("livro.txt")); 			
-		
+		BufferedReader in = new BufferedReader(new FileReader("livro.txt")); 				
 		for (int i=0; in.ready(); i++) 
 		{ 			
 			String linha = in.readLine();				
-			
-			switch(linha.substring(0, 2))
-			{
-			case "L ":				
-				if(livroUnico)
-					throw new RuntimeException("só pode haver um livro");
-				livroUnico = true;
-				livro = linha;
-				arvore.add("linha", null);				
-				break;
-			case "C ":
-				capitulo = linha;
-				emCapitulo(linha);				
-				break;
-			case "S ":
-				secao = linha;
-				emSecao(linha);				
-				break;
-			case "SS":
-				subsecao = linha;
-				emSubsecao(linha);
-				break;
-			case "P ":
-				paragrafo = linha;
-				emParagrafo(linha);				
-				break;
-			default:
-				throw new RuntimeException("Erro de leitura do tipo de linha!");				
-			}				
+			adiciona(linha);			
+							
 		}
 		in.close();	//fecha o arquivo	
+	
 		//===================================================================================					
 		/* TESTES QUE FUNCIONAM (precisa dos TADS Integer e não String:
 		 * 
@@ -86,14 +64,64 @@ public class App {
 		System.out.println("Posições Width:");
 		System.out.println(lista3.toString());	
 		*/
-	}	
+	}
+	public static void adiciona(String linha){
+		switch(linha.substring(0, 2))
+		{
+		case "L ":				
+			//if(livroUnico)
+			//	throw new RuntimeException("só pode haver um livro");
+			//livroUnico = true;
+			livro = linha;
+			arvore.add("linha", null);				
+			break;
+		case "C ":
+			capitulo = linha;
+			emCapitulo(linha);				
+			break;
+		case "S ":
+			secao = linha;
+			emSecao(linha);				
+			break;
+		case "SS":
+			subsecao = linha;
+			emSubsecao(linha);
+			break;
+		case "P ":
+			paragrafo = linha;
+			emParagrafo(linha);				
+			break;
+		default:
+			throw new RuntimeException("Erro de leitura do tipo de linha!");				
+		}
+	}
+	
+	public static void preenche() throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader("livro.txt")); 	
+		lista = new ArrayList<>();
+		for (int i=0; in.ready(); i++) 
+		{ 			
+			lista.add(in.readLine());								
+		}
+		in.close();
+		recursao(lista.get(0));
+		
+	}
+
+	public static void recursao(String linha){
+		
+		if(linha.substring(0, 2)=="C "){
+		//	arvore.add(linha, );			
+		}		
+		
+		
+	}
 	
 	public static void emCapitulo(String tipo){
 		System.out.println(capitulo+" está funcionando");
-		arvore.add(tipo, livro);
-						
-		
+		arvore.add(tipo, livro);		
 	}
+	
 	public static void emSecao(String tipo){
 		System.out.println(secao+" está funcionando");
 		
@@ -106,4 +134,5 @@ public class App {
 		System.out.println(paragrafo+" está funcionando");
 		
 	}
+	
 }
